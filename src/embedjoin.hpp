92ceb95be22bcc5a5183e7c915c33ec4ce2dbd65
marinele <eugenio.marinelli@eurecom.fr>
2020-07-09 10:24:22 +0200

    Add header file

---
+++ src/embedjoin.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <ctime>
+#include <unistd.h>
+#include <sys/time.h>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string.h>
+#include <time.h>
+#include <algorithm>
+#include <tuple>
+#include <numeric>
+#include <math.h>
+#include <map>
+#include <set>
+#include <chrono>
+
+
+
+int edit_distance(const char *x, const int x_len, const  char *y, const int y_len, int k);
+
