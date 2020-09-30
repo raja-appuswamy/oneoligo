fbcaeba7d1c5c77c75d506b70c5d5dfeb6ec4558
Eug9 <53082016+Eug9@users.noreply.github.com>
2020-09-30 12:52:11 +0200

    Cleaning

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,78 +2,61 @@
 
 using namespace std;
 
-
 void print_oristrings( char *oristrings, vector<int> len )
 {
-
 	char **tmp;
 	tmp=&oristrings;
-
 	string filename_output="oristrings.txt";
-
 	ofstream outFile;
 
 	outFile.open(filename_output, ios::out | ios::trunc);
 
 	if (outFile.is_open()) {
 		for(int i=0; i<NUM_STRING; i++){
-
 			for(int t=0; t<len[i]; t++){
-
 				outFile<<tmp[i][t];
-
 			}
 			outFile<<std::endl;
-
 			}
 	}
 };
 
 
-
-
-
-
-
 void print_embedded( char **output, int len_output, int batch_size, std::string filename ){
 
 	ofstream outFile;
-
 	outFile.open(filename, ios::out | ios::trunc);
 
 	if (outFile.is_open()) {
-
+		
 		for(int i=0; i<NUM_STRING; i++){
 			for(int j=0; j<NUM_STR; j++ ){
 				for(int k=0; k<NUM_REP; k++){
-
 					for(int t=0; t<len_output; t++){
 
 						if(output[(int)(i/batch_size)][ABSPOS((int)(i%batch_size),j,k,t,len_output)]==0){
 							break;
 						}
+						
 						outFile<<output[(int)(i/batch_size)][ABSPOS((int)(i%batch_size),j,k,t,len_output)];
-
 					}
 					outFile<<std::endl;
-
 				}
 			}
 		}
 	}
 };
 
+
 void print_buckets( vector<tuple<int,int,int,int,int>> &buckets, std::string filename){
 
 	ofstream outFile;
-
 	outFile.open(filename, ios::out | ios::trunc);
 
 	if (outFile.is_open()) {
+		
 		for(int i=0; i<buckets.size(); i++){
-
 			outFile<<get<0>(buckets[i])<<", "<<get<1>(buckets[i])<<", "<<get<2>(buckets[i])<<", "<<get<3>(buckets[i])<<", "<<get<4>(buckets[i])<<std::endl;
-
 		}
 	}
 };
@@ -81,17 +64,15 @@ void print_buckets( vector<tuple<int,int,int,int,int>> &buckets, std::string fil
 void print_candidate_pairs( vector<tuple<int,int,int,int,int,int>> &candidates, std::string filename ){
 
 	ofstream outFile;
-
 	outFile.open(filename, ios::out | ios::trunc);
 
 	if (outFile.is_open()) {
 		for(int i=0; i<candidates.size(); i++){
-
-			outFile<</*get<4>(candidates[i])<<", "<<get<5>(candidates[i])<<", "<<*/get<0>(candidates[i])<<", "<<get<1>(candidates[i])<<", "<<get<2>(candidates[i])<<", "<<get<3>(candidates[i])<<", "<<get<4>(candidates[i])<<", "<<get<5>(candidates[i])<<std::endl;
-
+			outFile<<get<0>(candidates[i])<<", "<<get<1>(candidates[i])<<", "<<get<2>(candidates[i])<<", "<<get<3>(candidates[i])<<", "<<get<4>(candidates[i])<<", "<<get<5>(candidates[i])<<std::endl;
 		}
 	}
 };
+
 
 void print_configuration(int batch_size,int n_batches, size_t len_output, int countfilter, int samplingrange){
 	std::cout<<"\nParameter selected:"<<std::endl;
