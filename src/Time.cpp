79c429053748abd16cf8b07b22c4806562a1ea85
Eug9 <53082016+Eug9@users.noreply.github.com>
2020-09-30 12:55:14 +0200

    Cleaning

--- src/Time.cpp
+++ src/Time.cpp
@@ -3,23 +3,17 @@
 class Time {
 
 private:
-
 	std::chrono::system_clock::time_point t=std::chrono::system_clock::now();
-
 	std::vector<std::chrono::system_clock::time_point> read_time; // 0,0,0
 	std::vector<std::chrono::system_clock::time_point> read_data_time; // 0,0,1
 	std::vector<std::chrono::system_clock::time_point> init_lsh_time; // 0,0,2
 	std::vector<std::chrono::system_clock::time_point> init_rev_lsh_time; // 0,0,3
-
-
+	
 	std::vector<std::chrono::system_clock::time_point> embeddata; // 0,1,0
 	std::vector<std::chrono::system_clock::time_point> embeddata_allocation_USM; // 0,1,1
 	std::vector<std::chrono::system_clock::time_point> embeddata_generate_random_string; // 0,1,2
 	std::vector<std::chrono::system_clock::time_point> embeddata_measure; // 0,1,3
 	std::vector<std::chrono::system_clock::time_point> embeddata_computation; // 0,1,4
-
-
-
 
 	std::vector<std::chrono::system_clock::time_point> create_buckets; // 0,2,0
 	std::vector<std::chrono::system_clock::time_point> create_buckets_measure; // 0,2,1
@@ -27,14 +21,11 @@ private:
 
 	std::vector<std::chrono::system_clock::time_point> sorting_buckets; // 0,3,0
 
-
 	std::vector<std::chrono::system_clock::time_point> init_cand; // 0,4,0
 	std::vector<std::chrono::system_clock::time_point> init_cand_compute_buckets_delim; // 0,4,1
 	std::vector<std::chrono::system_clock::time_point> init_cand_remove_one_elem_buckets; // 0,4,2
 	std::vector<std::chrono::system_clock::time_point> init_cand_resize; // 0,4,3
 	std::vector<std::chrono::system_clock::time_point> init_cand_scan_cand_vector; // 0,4,4
-
-
 
 	std::vector<std::chrono::system_clock::time_point> generate_cand; // 0,5,0
 	std::vector<std::chrono::system_clock::time_point> generate_cand_measure; // 0,5,1
@@ -54,7 +45,6 @@ private:
 	std::vector<std::chrono::system_clock::time_point> total_join_time; // 1,0,0
 	std::vector<std::chrono::system_clock::time_point> total_alg_time; // 2,0,0
 
-
 	double get_time(std::vector<std::chrono::system_clock::time_point> time){
 		long d=std::chrono::duration_cast<std::chrono::milliseconds>(time[1]-time[0]).count();
 		double t=(double)d/1000.0;
@@ -63,7 +53,6 @@ private:
 
 	template<class Func>
 	double find_case(int i, int main_phase, int phase, int subphase, Func to_do){
-
 
 			double res=0;
 
@@ -189,12 +178,8 @@ private:
 					}
 				}
 			}
-
 			return res;
-
 	}
-
-
 
 	double get_time_diff(int main_phase, int phase, int subphase){
 
@@ -203,31 +188,25 @@ private:
 			double t=this->get_time(time);
 			return t;
 		});
-
 		return res;
-
 	}
 
+	
 	void record_time(int i, int main_phase, int phase, int subphase){
-
 
 		find_case(i, main_phase, phase, subphase, [&](std::vector<std::chrono::system_clock::time_point> &time){
 			auto new_t=std::chrono::system_clock::now();
 			time[i]=new_t;
 			return 0;
 		});
-
-
 	}
 
-
-
+	
 public:
 	Time(){
-
 		this->read_time.resize(2); // 0,0,0
 		this->read_data_time.resize(2); // 0,0,1
-	    this->init_lsh_time.resize(2); // 0,0,2
+		this->init_lsh_time.resize(2); // 0,0,2
 		this->init_rev_lsh_time.resize(2); // 0,0,3
 
 		this->embeddata.resize(2); // 0,1,0
@@ -236,23 +215,17 @@ public:
 		this->embeddata_measure.resize(2); // 0,1,3
 		this->embeddata_computation.resize(2); // 0,1,4
 
-
-
-
 		this->create_buckets.resize(2); // 0,2,0
 		this->create_buckets_measure.resize(2); // 0,2,1
 		this->create_buckets_computation.resize(2); // 0,2,2
 
 		this->sorting_buckets.resize(2); // 0,3,0
 
-
 		this->init_cand.resize(2); // 0,4,0
 		this->init_cand_compute_buckets_delim.resize(2); // 0,4,1
 		this->init_cand_remove_one_elem_buckets.resize(2); // 0,4,2
 		this->init_cand_resize.resize(2); // 0,4,3
 		this->init_cand_scan_cand_vector.resize(2); // 0,4,4
-
-
 
 		this->generate_cand.resize(2); // 0,5,0
 		this->generate_cand_measure.resize(2); // 0,5,1
@@ -273,13 +246,14 @@ public:
 		this->total_alg_time.resize(2); // 2,0,0
 	}
 
+	
 	void print_report(std::string dev, int num_candidates, int num_outputs, std::ostream &out_file=std::cout){
 
 		out_file<<"Step,SubStep,Time(sec),Device"<<std::endl;
-
+		
 		double t=get_time(this->read_time);
 		out_file<<"Read Data,\t,"<<t<<std::endl;
-
+		
 		t=get_time(this->read_data_time);
 		out_file<<"\t,Read Data and initial sorting,"<<t<<std::endl;
 
@@ -364,8 +338,6 @@ public:
 		t=get_time(this->cand_proc_removing_duplicates_from_cand_to_verify);
 		out_file<<"\t,Removing duplicates,"<<t<<std::endl;
 
-
-
 		t=get_time(this->edit_dist);
 		out_file<<"Edit Distance,\t,"<<t<<std::endl;
 
@@ -379,27 +351,18 @@ public:
 		out_file<<"Number output,\t"<<num_outputs<<std::endl;
 	}
 
+	
 	void start_time(int main_phase, int phase, int subphase){
-
 		record_time(0, main_phase, phase, subphase );
-
 	}
 
 
 	void end_time(int main_phase, int phase, int subphase){
-
 		record_time(1, main_phase, phase, subphase );
-
 	}
 
 	double get_step_time(int main_phase, int phase, int subphase){
-
 		double t=get_time_diff(main_phase, phase, subphase);
 		return t;
 	}
-
-
-
 };
-
-
